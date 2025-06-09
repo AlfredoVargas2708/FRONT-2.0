e47220275023c236976c4711ffd3f3eef2fe4464
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-06-09 12:32:26 -0400

    feat: integrate Bootstrap and enhance home component with table for Lego pieces

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppService } from '../../services/app.service';
+import { tableHeaders } from '../../fields/tableHeaders';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { AppService } from '../../services/app.service';
 })
 export class Home implements OnInit {
   legoPieces: any[] = [];
+  tableHeaders = tableHeaders;
 
   constructor(private appService: AppService) { }
 
@@ -22,7 +24,8 @@ export class Home implements OnInit {
     this.appService.getLegoPieces().subscribe({
       next: (data) => {
         this.legoPieces = data;
-        this.loadImages();
+        this.loadSetImages();
+        this.loadPieceImage();
       },
       error: (error) => {
         console.error('Error fetching lego pieces:', error);
@@ -30,20 +33,29 @@ export class Home implements OnInit {
     })
   }
 
-  loadImages(): void {
+  loadSetImages(): void {
     this.appService.getSetImages().subscribe({
       next: (images) => {
         this.legoPieces.forEach(piece => {
           const image = images.find((img: any) => img.code_sets === piece.lego);
           if (image) {
-            piece.image = image.image_set;
+            piece.image_set = image.image_set;
           } else {
-            piece.image = 'https://assets.lego.com/logos/v4.5.0/brand-lego.svg';
+            piece.image_set = 'https://assets.lego.com/logos/v4.5.0/brand-lego.svg';
           }
         });
       },
       error: (error) => {
         console.error('Error fetching set images:', error);
+      }
+    });
+  }
+
+  loadPieceImage(): void {
+    this.legoPieces = this.legoPieces.map(piece => {
+      return {
+        ...piece,
+        image: `https://www.lego.com/cdn/product-assets/element.img.photoreal.192x192/${piece.code}.jpg`
       }
     });
   }
---
+++ src/fields/tableHeaders.ts
@@ -0,0 +1,12 @@
+export const tableHeaders = [
+    'Imagen',
+    'Código',
+    'Código Set',
+    'Lego Set',
+    'Imagen Set',
+    'Cantidad',
+    'Bolsa',
+    'Pedido',
+    'Completo',
+    'Reemplazado',
+    'Acciones']
